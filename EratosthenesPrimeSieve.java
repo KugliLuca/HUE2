9c2f58c39d19bf02bc85640e0cccb7f8b8f2953e
Luca <luca.abgefact@gmail.com>
2022-09-22 09:29:35 +0100

    isPrime() finished and printPrimes() rewritten

--- EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve.java
@@ -7,21 +7,33 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @java.lang.Override
     public boolean isPrime(int p) {
-        return false;
-    }
-
-    @java.lang.Override
-    public void printPrimes() {
         for (int i = 0; i < cons; i++){
-            arr.add(new Boolean(false));
+            arr.add(false);
         }
 
         for (int i = 2; i < cons; i++){
             if(arr.get(i) != true){
-                System.out.println(i + "\n" + "\r");
+
                 for(int j = i; (j * i) < cons; j++){
                     arr.set(j * i, true);
                 }
+            }
+        }
+
+        if(arr.get(p) == false){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @java.lang.Override
+    public void printPrimes() {
+        for (int i = 2; i < cons; i++){
+            if(!arr.get(i)){
+                System.out.println(i + ", ");
+            }else{
+
             }
         }
 
