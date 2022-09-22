3f3dfa9afc71e349af284f8b2f5ec122a241d9bf
Luca <luca.abgefact@gmail.com>
2022-09-22 09:32:36 +0100

    Project finalized

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ import java.awt.print.PrinterException;
 public class Main {
     public static void main(String[] args){
         EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve();
-
+        System.out.println(eps.isPrime(2));
+        System.out.println(eps.isPrime(3));
+        System.out.println(eps.isPrime(4));
         eps.printPrimes();
     }
 }
