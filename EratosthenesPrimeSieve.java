a369a737118e13259766fa54afd1b742355eb92a
Luca <luca.abgefact@gmail.com>
2022-09-22 09:19:30 +0100

    printPrimes() fixed

--- EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve.java
@@ -12,16 +12,19 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @java.lang.Override
     public void printPrimes() {
-        for (boolean b: arr) {b = false;};
+        for (int i = 0; i < cons; i++){
+            arr.add(new Boolean(false));
+        }
 
         for (int i = 2; i < cons; i++){
-            if(arr.get(i) != false){
+            if(arr.get(i) != true){
                 System.out.println(i + "\n" + "\r");
                 for(int j = i; (j * i) < cons; j++){
-                    arr.set(j * i, false);
+                    arr.set(j * i, true);
                 }
             }
         }
+
 
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-
+import java.awt.print.PrinterException;
 
 public class Main {
     public static void main(String[] args){
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve();
 
+        eps.printPrimes();
     }
 }
